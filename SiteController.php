603f8abefc62be3bdbcecd72031708bda51e3efc
surajkumar3433 <57474670+surajkumar3433@users.noreply.github.com>
2019-11-20 22:58:54 +0530

    Add files via upload

--- SiteController.php
+++ SiteController.php
@@ -65,7 +65,7 @@ class SiteController extends Controller
 		
     }
 	public function actionCreatewidget(){
-        return $this->render('hellowidget');
+        //return $this->render('hellowidget');
     }
 
     /**
@@ -128,5 +128,26 @@ class SiteController extends Controller
     public function actionAbout()
     {
         return $this->render('about');
+    }
+
+    public function actionGetData($amount,$from_currency,$to_currency)
+    {
+        $apikey = '441be14e3ba8f42fe331';
+
+  $from_Currency = urlencode($from_currency);
+  $to_Currency = urlencode($to_currency);
+  $query =  "{$from_Currency}_{$to_Currency}";
+
+  $json = file_get_contents("https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&apiKey={$apikey}");
+  $obj = json_decode($json, true);
+
+  $val = floatval($obj["$query"]);
+
+
+  $total = $val * $amount;
+  return number_format($total, 2, '.', '');
+
+
+
     }
 }
